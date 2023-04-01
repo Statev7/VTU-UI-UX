9bd4846150d5bf268e0b267e06f863b029ccc808
Statev07 <petiostatev2703@gmail.com>
2023-04-01 12:11:20 +0300

    Implement UI.

---
+++ scripts/btn.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("go-top-btn");
+btn.addEventListener('click', topFunction);
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
